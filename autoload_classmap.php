3744e96c6626a507f1f4ba30ddc655b36146a64b
JonConnerDotCom <jon.conner@live.com>
2012-12-04 21:57:59 -0500

    update

---
+++ autoload_classmap.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'JcdcFoundation\Module'                          => __DIR__ . '/Module.php',
+    'JcdcFoundation\Form\View\Helper\FoundationForm' => __DIR__ . '/src/JcdcFoundation/Form/View/Helper/FoundationForm.php',
+    'JcdcFoundation\View\Helper\Alert'               => __DIR__ . '/src/JcdcFoundation/View/Helper/Alert.php',
+    'JcdcFoundation\View\Helper\Label'               => __DIR__ . '/src/JcdcFoundation/View/Helper/Label.php',
+    'JcdcFoundation\View\Helper\Tooltip'             => __DIR__ . '/src/JcdcFoundation/View/Helper/Tooltip.php',
+);
